7fdf7aa6c5ee6e9786889a55f91fc2d2b338c4d4
deletrics <deletrics@gmail.com>
2026-07-27 03:38:51 +0000

    fix(chat): remove duplicate RefreshCw function declaration

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -353,25 +353,3 @@ export default function ChatPage() {
     </div>
   );
 }
-
-function RefreshCw(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
-      <path d="M3 3v5h5" />
-      <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16" />
-      <path d="M21 21v-5h-5" />
-    </svg>
-  )
-}
